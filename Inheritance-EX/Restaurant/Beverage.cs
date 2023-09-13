84adaa70eb7551a01fd99cc34eb757e5b578f19c
Stoqn <podlobg@gmail.com>
2023-09-13 10:20:02 +0300

    up

---
+++ Inheritance-EX/Restaurant/Beverage.cs
@@ -0,0 +1,13 @@
+﻿namespace NeedForSpeed
+{
+    public class Beverage : Product
+    {
+        public Beverage(string name, decimal price, double milliliters)
+            : base(name, price)
+        {
+            Mililiters = milliliters;
+        }
+
+        public double Mililiters { get; private set; }
+    }
+}
