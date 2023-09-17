942afcf7685790f9a9927c929d308be491fc9ef4
Stoqn <podlobg@gmail.com>
2023-09-17 17:14:07 +0300

    up

---
+++ Encapsolation -EX/BoxData/Class-Box-Data/Models/Box.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ClassBoxData.Models
+{
+    public class Box
+    {
+        private const string PropZeroOrNegativeExMessage = "{0} cannot be zero or negative.";
+
+        private double lenght;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Lenght
+        {
+            get => lenght;
+           private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(PropZeroOrNegativeExMessage, nameof(Lenght)));
+                }
+
+                lenght = value;
+            }
+        }
+        public double Width
+        {
+            get => width;
+          private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(PropZeroOrNegativeExMessage, nameof(Width)));
+                }
+
+                width = value;
+            }
+        }  
+        public double Height
+        {
+            get => height;
+           private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(PropZeroOrNegativeExMessage, nameof(Height)));
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+           => 2 * Lenght * Width + LateralSurfaceArea();
+
+        public double LateralSurfaceArea()
+            => 2 * Lenght * Height + 2 * Width * Height;
+
+        public double Volume()
+            => Lenght * Width * Height;
+    }
+}
