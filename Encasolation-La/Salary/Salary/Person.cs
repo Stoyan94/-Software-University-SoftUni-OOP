db6b59f1b52d9b66b32132926cdb98d34884b119
Stoqn <podlobg@gmail.com>
2023-09-18 11:06:21 +0300

    ex

---
+++ Encasolation-La/Salary/Salary/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName { get { return firstName; } private set { firstName = value; } }
+        public string LastName { get { return lastName; } private set { lastName = value; } }
+        public int Age { get { return age; } private set { age = value; } }
+        public decimal Salary { get { return salary; } private set { salary = value; } }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        => $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age > 30)
+                this.salary += this.salary * percentage / 100;
+            else
+                this.salary += this.salary * percentage / 200;
+        }
+
+    }
+}
---
+++ Encasolation-La/Salary/Salary/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using PersonsInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+         
+        }
+
+    }
+}
