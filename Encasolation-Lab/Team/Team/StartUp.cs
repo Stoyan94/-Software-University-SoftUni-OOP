036425c5d4f5b0281c6e3212cab51af1c1e7b8e0
Stoqn <podlobg@gmail.com>
2023-09-18 12:11:33 +0300

    ex

---
+++ Encasolation-Lab/Team/Team/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PersonsInfo;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int players = int.Parse(Console.ReadLine());
+
+        Team team = new Team("SoftUni");
+
+        for (int i = 0; i < players; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string firstName = input[0];
+            string lastName = input[1];
+            int age = int.Parse(input[2]);
+            decimal salary = decimal.Parse(input[3]);
+
+            Person person = new Person(firstName, lastName, age, salary);
+
+            team.Add(person);
+        }
+    }
+}
---
+++ Encasolation-Lab/Team/Team/Team.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public List<Person> FirstTeam => firstTeam;
+
+        public List<Person> ReserveTeam => reserveTeam; 
+
+        public void Add(Person person)
+        {
+            if (person.Age <40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
