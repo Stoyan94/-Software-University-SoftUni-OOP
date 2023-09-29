d91c2177c50dd59bd36b8faf5e34e426f93b3292
Stoqn <podlobg@gmail.com>
2023-09-29 16:10:56 +0300

    ex

---
+++ Interfaces and Abstraction-LAB/InterfaceInheritance/InterfaceInheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+interface Readable
+{
+    string Read();
+}
+
+interface Writable : Readable
+{
+    void Write(string text);
+}
+
+class Printer : Writable
+{
+    public string Read()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Write(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
