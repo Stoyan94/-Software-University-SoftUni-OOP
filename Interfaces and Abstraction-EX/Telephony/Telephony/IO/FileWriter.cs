dee3a770823364b3fe1e9cd88f9c60b763eb2a24
Stoqn <podlobg@gmail.com>
2023-10-08 13:45:02 +0300

    up

---
+++ Interfaces and Abstraction-EX/Telephony/Telephony/IO/FileWriter.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Telephony.IO.Interfaces;
+
+namespace Telephony.IO
+{
+    public class FileWriter : IWriter
+    {
+        public void WriteLine(string line)
+        {
+            string filePath = "../../../test.txt";
+
+            using StreamWriter sw = new StreamWriter(filePath, true);
+
+           sw.WriteLine(line);
+        }
+    }
+}
