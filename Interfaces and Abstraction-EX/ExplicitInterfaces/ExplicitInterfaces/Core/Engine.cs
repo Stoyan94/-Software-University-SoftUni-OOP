bef824a5882b73972da49768c2b5e680ed1423e9
Stoqn <podlobg@gmail.com>
2023-10-14 19:42:31 +0300

    fn

--- Interfaces and Abstraction-EX/ExplicitInterfaces/ExplicitInterfaces/Core/Engine.cs
+++ Interfaces and Abstraction-EX/ExplicitInterfaces/ExplicitInterfaces/Core/Engine.cs
@@ -10,7 +10,27 @@ namespace ExplicitInterfaces.Core
     {
         public void Run()
         {
-           
+            List<Citizen> citizens = new();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                citizens.Add(new Citizen(tokens[0], tokens[1], int.Parse(tokens[2])));
+            }
+
+            foreach (var citizen in citizens)
+            {
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
         }
     }
 }
