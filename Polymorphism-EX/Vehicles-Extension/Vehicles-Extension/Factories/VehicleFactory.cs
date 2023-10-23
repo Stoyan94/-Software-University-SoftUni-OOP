7c644ff3906b2f7f04f90a53abb91d448d465d55
Stoqn <podlobg@gmail.com>
2023-10-23 19:13:57 +0300

    fn

---
+++ Polymorphism-EX/Vehicles-Extension/Vehicles-Extension/Factories/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vehicles_Extension.Factories.Interfaces;
+using Vehicles_Extension.Models;
+using Vehicles_Extension.Models.Interfaces;
+
+namespace Vehicles_Extension.Factories
+{
+    public class VehicleFactory : IVehicleFactory
+    {
+        public IVehicle Create(string type, double fuelQuantity, double fuelConsumption, double tankCapacity)
+        {
+            switch (type)
+            {
+                case "Car":
+                    return new Car(fuelQuantity, fuelConsumption, tankCapacity);
+                case "Truck":
+                    return new Truck(fuelQuantity, fuelConsumption, tankCapacity);
+                case "Bus":
+                    return new Bus(fuelQuantity, fuelConsumption, tankCapacity);
+                default:
+                    throw new ArgumentException("Invalid vehicle type");
+            }
+        }
+    }
+}
