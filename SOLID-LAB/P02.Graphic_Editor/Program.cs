92efa3d1ac4e936a3ff0fa33c3f0d8def3b39d4c
Stoqn <podlobg@gmail.com>
2023-10-26 14:23:48 +0300

    ex

---
+++ SOLID-LAB/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            IShape circle = new Circle();
+            IShape rectangle = new Rectangle();
+            IShape square = new Square();
+
+            Console.WriteLine(circle.GetShape());
+            Console.WriteLine(rectangle.GetShape());
+            Console.WriteLine(square.GetShape());
+        }
+    }
+}
