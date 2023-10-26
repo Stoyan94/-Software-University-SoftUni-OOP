14fe0fc8da48e668b6fbbb2ceb422d72b93e51be
Stoqn <podlobg@gmail.com>
2023-10-26 14:58:21 +0300

    ex

---
+++ SOLID-LAB/P03.Detail_Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Stoyan");
+
+            IList<string> list = new List<string>();
+            list.Add("CEO");
+
+            Manager manager = new Manager("Stoyan", list);
+
+            List<Employee> list2 = new List<Employee>();
+            list2.Add(employee);
+            list2.Add(manager);
+
+            DetailsPrinter newDetail = new DetailsPrinter(list2);
+            newDetail.PrintDetails();
+            
+        }
+    }
+}
