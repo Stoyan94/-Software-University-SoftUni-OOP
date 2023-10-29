0137f25b1803b2e75ce3b46b59d67c995c85e742
Stoqn <podlobg@gmail.com>
2023-10-29 11:16:42 +0200

    up

---
+++ SOLID-EX/LogForU/LogForU.Core/Utils/DateTimeValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace LogForU.Core.Utils
+{
+    public static class DateTimeValidator
+    {
+        public static bool ValidateDateime(string dateTime)
+        {
+            if (DateTime.TryParseExact(dateTime
+                , "M/dd/yyyy h:mm:ss tt"
+                , CultureInfo.InvariantCulture
+                , DateTimeStyles.None
+                , out DateTime result))
+            {
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
