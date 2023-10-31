910e26014dcbde560585766d57e1ece928aad1ce
Stoqn <podlobg@gmail.com>
2023-10-31 10:34:49 +0200

    up

---
+++ SOLID-EX/LogForU/LogForU.Core/Exceptions/EmptyFileNameException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LogForU.Core.Exceptions
+{
+    public class EmptyFileNameException : Exception
+    {
+        private const string DefaultMessage = "Created time of message cannot be null or whitespace";
+        public EmptyFileNameException() 
+            : base(DefaultMessage)
+        {
+            
+        }
+
+        public EmptyFileNameException(string message) 
+            : base(message) 
+        {
+            
+        }
+    }
+}
