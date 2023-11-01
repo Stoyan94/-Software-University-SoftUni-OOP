d6fea7785336cd9244375611d5cb2cee947760a0
Stoqn <podlobg@gmail.com>
2023-11-01 14:34:36 +0200

    up

---
+++ SOLID-EX/LogForU/LogForU/CustomLayots/XmlLayout.cs
@@ -0,0 +1,23 @@
+﻿using LogForU.Core.Layouts.Interfaces;
+using System.Text;
+
+namespace LogForU.CustomLayots
+{
+    public class XmlLayout : ILayout
+    {
+        public string Format
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("<log>");
+                sb.AppendLine("    <data>{0}{date}");
+                sb.AppendLine("    <level>{1}{/level}");
+                sb.AppendLine("    <message>{2}{/message}");
+                sb.AppendLine("</log>");
+
+                return sb.ToString().TrimEnd();
+            }
+        }
+    }
+}
