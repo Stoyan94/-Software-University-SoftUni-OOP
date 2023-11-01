2bdab7a704f59ea17d44394bf8905d084d145e3d
Stoqn <podlobg@gmail.com>
2023-11-01 14:52:01 +0200

    rw

--- SOLID-EX/LogForU/LogForU/CustomLayots/XmlLayout.cs
+++ SOLID-EX/LogForU/LogForU/CustomLayots/XmlLayout.cs
@@ -11,9 +11,9 @@ namespace LogForU.CustomLayots
             {
                 StringBuilder sb = new StringBuilder();
                 sb.AppendLine("<log>");
-                sb.AppendLine("    <data>{0}{date}");
-                sb.AppendLine("    <level>{1}{/level}");
-                sb.AppendLine("    <message>{2}{/message}");
+                sb.AppendLine("    <data>{0}>date>");
+                sb.AppendLine("    <level>{1}</level>");
+                sb.AppendLine("    <message>{2}/message");
                 sb.AppendLine("</log>");
 
                 return sb.ToString().TrimEnd();
--- SOLID-EX/LogForU/LogForU/Program.cs
+++ SOLID-EX/LogForU/LogForU/Program.cs
@@ -6,15 +6,18 @@ using LogForU.Core.Layouts.Interfaces;
 using LogForU.Core.Loggers;
 using LogForU.Core.Loggers.Interfaces;
 using LogForU.Core.Utils;
+using LogForU.CustomLayots;
 
-var simpleLayout = new SimpleLayout();
-var consoleAppender = new ConsoleAppender(simpleLayout);
+var xmlLayout = new XmlLayout();
+var consoleAppender = new ConsoleAppender(xmlLayout);
 
 var file = new LogFile();
-var fileAppender = new FileAppender(simpleLayout, file);
+var fileAppender = new FileAppender(xmlLayout, file);
 
-var logger = new Logger(consoleAppender, fileAppender);
-logger.Error("3/26/2015 2:08:11 PM", "Error parsing JSON.");
-logger.Info("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
+var logger = new Logger(fileAppender);
+
+logger.Fatal("3/31/2015 5:23:54 PM", "mscorlib.dll does not respond");
+logger.Critical("3/31/2015 5:23:54 PM", "No connection string found in App.config");
+
 
 
