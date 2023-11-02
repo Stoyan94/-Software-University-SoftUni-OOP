a936b88adaa62ed6b9140ae31086a7433eeb8bbc
Stoqn <podlobg@gmail.com>
2023-11-02 15:37:17 +0200

    up

--- UNIT TESTING/AxeTest/Skeleton.Tests/DummyTests.cs
+++ UNIT TESTING/AxeTest/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,88 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void AxeShouldInitlizeWithCorrectValues()
         {
+            //Arrange and act
+            Dummy dummy = new Dummy(100,100);
 
+            //Assert
+            Assert.AreEqual(100, dummy.Health);           
+        }
+
+        [Test]
+
+        public void TakeAtackShouldDecreaseHealth()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(100,100);
+
+            //Act
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.AreEqual(50, dummy.Health);
+        }
+
+        [Test]
+        public void TakeAtackShouldThrowExceptionIfDummyIsDead()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(100,100);
+
+            //Act
+            dummy.TakeAttack(50);
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(
+                () => dummy.TakeAttack(50),
+                "Dummy is dead.");
+        }
+
+        [Test]
+        
+        public void GiveExpShouldReturnCurrentExperinceIfDummyIsDead()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(100, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.AreEqual(100, dummy.GiveExperience());
+        }    
+        
+        [Test]
+        
+        public void GiveExpShouldThrowAnExceptionIfDummyIsNotDead()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(100, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+       
+            //Assert
+            Assert.Throws<InvalidOperationException>(
+                () => dummy.GiveExperience(),
+                "Targer is not dead.");
+        }
+
+        [Test]
+
+        public void IsDeadShouldChekIfHealthIsBelowOrEqualToZero()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(50, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.IsTrue(dummy.IsDead());
         }
     }
 }
