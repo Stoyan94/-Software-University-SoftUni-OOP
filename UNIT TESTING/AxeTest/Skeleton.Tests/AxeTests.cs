5c1b099dd20a73b742edc5da4ba855f5620d41e8
Stoqn <podlobg@gmail.com>
2023-11-02 14:54:16 +0200

    up

--- UNIT TESTING/AxeTest/Skeleton.Tests/AxeTests.cs
+++ UNIT TESTING/AxeTest/Skeleton.Tests/AxeTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,37 @@ namespace Skeleton.Tests
     public class AxeTests
     {
         [Test]
-        public void Test1()
+        public void AtackMethodShouldDecreaseDurabilityPoint()
         {
+            //Arrange
+            Dummy target = new Dummy(10, 10);
+            Axe axe = new Axe(100, 10);
 
+            //Act
+            axe.Attack(target);
+
+            //Assert
+            Assert.AreEqual(9, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void AtackMethodShouldThrowExceptionIfDurabilityIsZero() 
+        {
+            //Arrange
+            Dummy target = new Dummy(10, 10);
+            Axe axe = new Axe(10, 10);
+
+            //Act
+            for (int i = 0; i < 10; i++)
+            {
+                axe.Attack(target);
+            }
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(
+                () => axe.Attack(target), "Axe is broken."); 
+
+            
         }
     }
 }
