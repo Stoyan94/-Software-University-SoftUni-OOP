5b0a806819d812be73fe897ad2aff87f2135a58d
Stoqn <podlobg@gmail.com>
2023-11-03 23:14:42 +0200

    up/rw

--- Interfaces and Abstraction-EX/MilitaryElite/MilitaryElite/IO/ConsoleWriter.cs
+++ Interfaces and Abstraction-EX/MilitaryElite/MilitaryElite/IO/ConsoleWriter.cs
@@ -6,14 +6,9 @@
 
     public class ConsoleWriter : IWriter
     {
-        public void Write(string text)
-        {
-            Console.Write(text);
-        }
+        public void Write(string text) => Console.Write(text);
+        
 
-        public void WriteLine(string text)
-        {
-            Console.WriteLine(text);
-        }
+      
     }
 }
