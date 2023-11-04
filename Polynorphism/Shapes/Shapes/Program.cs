cf0504df03eb1bf9ef1054cb45ede39ad8f1dc90
Stoqn <podlobg@gmail.com>
2023-11-04 21:23:34 +0200

    ex

---
+++ Polynorphism/Shapes/Shapes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Circle circle = new Circle(3);
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
