19b23feb1026608926fc08798249df8922a4e0ac
Stoqn <podlobg@gmail.com>
2023-11-06 15:03:26 +0200

    ex

---
+++ Interfaces and Abstraction-LAB/Cars/Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start() => "Engine start";
+
+        public string Stop() => $"Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine($"{this.Color} {GetType().Name} {this.Model}");
+            output.AppendLine(Start());
+            output.AppendLine(Stop());
+
+            return output.ToString().Trim();
+        }
+    }
+}
---
+++ Interfaces and Abstraction-LAB/Cars/Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public int Battery { get; private set; }
+
+        public string Start() => "Engine start";
+
+        public string Stop() => $"Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+
+            output.AppendLine($"{this.Color} {GetType().Name} {this.Model} with {this.Battery} Batteries");
+            output.AppendLine(Start());
+            output.AppendLine(Stop());
+
+            return output.ToString().Trim();
+        }
+    }
+}
