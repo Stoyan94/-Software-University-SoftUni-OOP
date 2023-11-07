5b531b6abee8296cb34ce0d3a376f472676463bb
Stoqn <podlobg@gmail.com>
2023-11-07 14:34:56 +0200

    up

--- UNIT TESTING-EX/Database.Tests/DatabaseTests.cs
+++ UNIT TESTING-EX/Database.Tests/DatabaseTests.cs
@@ -1,10 +1,52 @@
 namespace Database.Tests
 {
     using NUnit.Framework;
-
-    [TestFixture]
+    using System;
+    
+    [TestFixture]        
     public class DatabaseTests
     {
+        private Database database;
 
+        [SetUp]
+        public void SetUp()
+        {
+            database = new Database(1,2);
+        }
+
+        [Test]
+        public void CreatingDatabaseCountShouldBeCorrect()
+        {
+            //Arrange
+            int actualResult = database.Count;
+            int expectedResult = 2;
+
+            //Act
+            //database = new Database(1, 2); - moved to Setup
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 })]
+        public void CreatingDatabaseShouldAddElementsCorrectly(int[] data)
+        {
+            database = new Database(data);
+
+            int[] actualResult = database.Fetch();
+
+            Assert.AreEqual(data, actualResult);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 })]
+        public void CreatingDatabaseShouldThrowExceptionWhenCountIsMoreThan16(int[] data)
+        {
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(()
+                => database = new Database(data));
+
+            Assert.AreEqual("Array's capacity must be exactly 16 integers!", exception.Message);
+        }
     }
 }
