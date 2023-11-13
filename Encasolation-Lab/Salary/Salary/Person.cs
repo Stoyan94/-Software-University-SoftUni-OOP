38a9beb35c0a70830de624ae5ca6620b6939a35d
Stoqn <podlobg@gmail.com>
2023-11-13 14:17:28 +0200

    rw

--- Encasolation-Lab/Salary/Salary/Person.cs
+++ Encasolation-Lab/Salary/Salary/Person.cs
@@ -2,34 +2,50 @@
 {
     public class Person
     {
-        private string firstName;
-        private string lastName;
         private int age;
         private decimal salary;
-
-        public string FirstName { get { return firstName; } private set { firstName = value; } }
-        public string LastName { get { return lastName; } private set { lastName = value; } }
-        public int Age { get { return age; } private set { age = value; } }
-        public decimal Salary { get { return salary; } private set { salary = value; } }
-
+        private const int MinAge = 30;
         public Person(string firstName, string lastName, int age, decimal salary)
         {
-            this.FirstName = firstName;
-            this.LastName = lastName;
-            this.Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
             this.Salary = salary;
         }
 
-        public override string ToString()
-        => $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
-
-        public void IncreaseSalary(decimal percentage)
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age 
         {
-            if (this.age > 30)
-                this.salary += this.salary * percentage / 100;
-            else
-                this.salary += this.salary * percentage / 200;
+            get => age; 
+            private set{ age = value; }
         }
+        public decimal Salary 
+        {
+            get { return salary; }
+            private set {salary = value; }
+        }
+
+
+        //public void IncreaseSalary(decimal percentage)
+        //{
+        //    if (age < MinAge)
+        //    {
+        //        percentage /= 2;
+        //    }
+
+        //    this.Salary += this.Salary *(percentage / 100);
+            
+        //}
+
+        public decimal IncreaseSalary(decimal percentage) => age < MinAge
+            ? this.Salary += this.Salary * (percentage / 200) 
+            : this.Salary += this.Salary * (percentage / 100);
+
+        
+        public override string ToString( )
+            => $"{FirstName} {LastName} receives {salary:f2} leva.";
+        
 
     }
 }
