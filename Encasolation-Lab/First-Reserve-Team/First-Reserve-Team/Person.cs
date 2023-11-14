7c5e6da3dd5f0a2878055c043127f30d32444321
Stoqn <podlobg@gmail.com>
2023-11-14 17:41:50 +0200

    rw

---
+++ Encasolation-Lab/First-Reserve-Team/First-Reserve-Team/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private const int MinAgeBouns = 30;
+
+        private string firstName;
+        private string lasttName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (value.Length < ExceptionMessages.MinDigitsNames)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.FirstName, ExceptionMessages.MinDigitsNames));
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lasttName;
+            private set
+            {
+                if (value.Length < ExceptionMessages.MinDigitsNames)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.LastName, ExceptionMessages.MinDigitsNames));
+                }
+                lasttName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                if (value <= ExceptionMessages.MinAge)
+                {
+                    throw new ArgumentException(ExceptionMessages.AgeZero);
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => salary;
+            private set
+            {
+                if (value < ExceptionMessages.MinSalary)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.Salary, ExceptionMessages.MinSalary));
+                }
+                salary = value;
+            }
+        }
+
+        public decimal IncreaseSalary(decimal percentage) => age < MinAgeBouns
+            ? this.Salary += this.Salary * (percentage / 200)
+            : this.Salary += this.Salary * (percentage / 100);
+
+
+        public override string ToString()
+            => $"{FirstName} {LastName} receives {salary:f2} leva.";
+
+
+    }
+}
---
+++ Encasolation-Lab/First-Reserve-Team/First-Reserve-Team/Team.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private const int AgeSort = 40;
+
+        private string teamName;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        private Team()
+        {
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public Team(string teamName)
+            : this()
+        {
+            this.TeamName = teamName;
+        }
+
+
+        public string TeamName { get; }
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam;
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam;
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < AgeSort)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }           
+        }
+       
+    }
+}
