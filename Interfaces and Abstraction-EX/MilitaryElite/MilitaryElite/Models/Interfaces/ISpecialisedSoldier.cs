4370789f3953bf92073354c70a11985bf2acb289
Stoqn <podlobg@gmail.com>
2023-11-19 19:58:57 +0200

    rw

--- Interfaces and Abstraction-EX/MilitaryElite/MilitaryElite/Models/Interfaces/ISpecialisedSoldier.cs
+++
@@ -1,9 +0,0 @@
-﻿using MilitaryElite.Models.Enums;
-
-namespace MilitaryElite.Models.Interfaces
-{
-    public interface ISpecialisedSoldier : IPrivate
-    {
-        Corps Corps { get; }
-    }
-}
