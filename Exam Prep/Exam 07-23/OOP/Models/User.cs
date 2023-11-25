005de2e0f5b83821f44715f263cdf8a6c1d041ce
Stoqn <podlobg@gmail.com>
2023-11-25 19:08:33 +0200

    up

---
+++ Exam Prep/Exam 07-23/OOP/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using EDriveRent.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private double rating;
+        private string drivingLicenseNumber;
+
+        public string FirstName
+        {
+            get => firstName;
+
+            private set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName 
+        {
+            get => lastName;
+            private set
+            {
+                lastName = value;
+            }
+        }
+
+        public double Rating 
+        { 
+            get => rating;
+            private set
+            {
+                rating = value;
+            }
+        }
+
+        public string DrivingLicenseNumber 
+        { 
+            get => drivingLicenseNumber; 
+            private set => drivingLicenseNumber = value; }
+
+        public bool IsBlocked { get; private set; }
+
+        public void DecreaseRating()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseRating()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
