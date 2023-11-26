d2afc70ed4533ff7f3fa025314b2fb19a417a299
Stoqn <podlobg@gmail.com>
2023-11-26 18:24:50 +0200

    up

---
+++ Exam Prep/Exam 07-23/OOP/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using EDriveRent.Models;
+using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDriveRent.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private ICollection<User> userRepository;
+
+        private UserRepository()
+        {
+            userRepository = new List<User>();
+        }
+        public void AddModel(User model)
+        {
+            userRepository.Add(model);
+        }
+
+        public User FindById(string identifier)
+        {
+            var currUser = userRepository.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+
+            if (currUser is null)
+            {
+                return null;
+            }
+
+            return currUser;
+        }
+
+        public IReadOnlyCollection<User> GetAll() 
+            => (IReadOnlyCollection<User>) userRepository;
+        
+
+        public bool RemoveById(string identifier)
+        {
+            var currUser = userRepository.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+
+            if (currUser is null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
