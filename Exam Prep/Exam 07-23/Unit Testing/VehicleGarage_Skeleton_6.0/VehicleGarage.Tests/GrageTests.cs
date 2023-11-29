c7464fc44bd6d782d92c53ba57b48a73075b557f
Stoqn <podlobg@gmail.com>
2023-11-29 10:39:18 +0200

    up

--- Exam Prep/Exam 07-23/Unit Testing/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/GrageTests.cs
+++ Exam Prep/Exam 07-23/Unit Testing/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/GrageTests.cs
@@ -66,5 +66,37 @@ namespace VehicleGarage.Tests
             Assert.IsFalse(result);
             Assert.IsFalse(containts);
         }
+
+        [Test]
+        public void ChargeVehicleShouldChargeAllVehicleUnderTheGivenValue()
+        {
+            Garage garage = new Garage(10);
+
+            Vehicle vehicle = new Vehicle("Nisan", "GTR", "BS5555BS");
+            Vehicle vehicle2 = new Vehicle("Nisan", "Godzile", "BS5655BS");
+            Vehicle vehicle3 = new Vehicle("Nisan", "Aplha12", "BS5557BS");
+            Vehicle vehicle4 = new Vehicle("Nisan", "Aplha16", "BS5577BS");
+            Vehicle vehicle5 = new Vehicle("Nisan", "Aplha18", "BS5584BS");
+
+            garage.AddVehicle(vehicle);
+            garage.AddVehicle(vehicle2);
+            garage.AddVehicle(vehicle3);
+            garage.AddVehicle(vehicle4);
+            garage.AddVehicle(vehicle5);
+
+            garage.DriveVehicle(vehicle.LicensePlateNumber, 90, false);
+            garage.DriveVehicle(vehicle2.LicensePlateNumber, 70, false);
+            garage.DriveVehicle(vehicle3.LicensePlateNumber, 50, false);
+            garage.DriveVehicle(vehicle4.LicensePlateNumber, 10, false);
+
+            var chargeVehicle = garage.ChargeVehicles(50);
+
+            Assert.AreEqual(chargeVehicle, 3);
+            Assert.AreEqual(vehicle.BatteryLevel, 100);
+            Assert.AreEqual(vehicle2.BatteryLevel, 100);
+            Assert.AreEqual(vehicle3.BatteryLevel, 100);
+            Assert.AreEqual(vehicle4.BatteryLevel, 90);
+            Assert.AreEqual(vehicle5.BatteryLevel, 100);
+        }
     }
 }
