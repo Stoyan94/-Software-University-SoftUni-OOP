f34fd9057c285dfc8d48175f0e80c3e9bdf71a48
Stoqn <podlobg@gmail.com>
2023-11-29 10:56:53 +0200

    fn

--- Exam Prep/Exam 07-23/Unit Testing/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/GrageTests.cs
+++ Exam Prep/Exam 07-23/Unit Testing/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/GrageTests.cs
@@ -98,5 +98,68 @@ namespace VehicleGarage.Tests
             Assert.AreEqual(vehicle4.BatteryLevel, 90);
             Assert.AreEqual(vehicle5.BatteryLevel, 100);
         }
+
+        [Test]
+
+        public void DriveVehicleShouldNotMoveIfBateryDraingeIsAbove100rHigherOfOurBattery()
+        {
+            Garage garage = new Garage(10);
+
+            Vehicle vehicle = new Vehicle("Nisan", "GTR", "BS5555BS");
+            Vehicle vehicle2 = new Vehicle("Nisan", "Godzile", "BS5655BS");
+            Vehicle vehicle3 = new Vehicle("Nisan", "Aplha12", "BS5557BS");
+            Vehicle vehicle4 = new Vehicle("Nisan", "Aplha16", "BS5577BS");
+            Vehicle vehicle5 = new Vehicle("Nisan", "Aplha18", "BS5584BS");
+
+            garage.AddVehicle(vehicle);
+            garage.AddVehicle(vehicle2);
+            garage.AddVehicle(vehicle3);
+            garage.AddVehicle(vehicle4);
+            garage.AddVehicle(vehicle5);
+
+            garage.DriveVehicle(vehicle.LicensePlateNumber, 120, false);
+
+            garage.DriveVehicle(vehicle2.LicensePlateNumber, 50, false);
+            garage.DriveVehicle(vehicle2.LicensePlateNumber, 60, false);
+
+            garage.DriveVehicle(vehicle3.LicensePlateNumber, 50, true);
+            garage.DriveVehicle(vehicle3.LicensePlateNumber, 10, true);
+
+            var resultDamage = vehicle3.IsDamaged;
+
+            Assert.AreEqual(100, vehicle.BatteryLevel);
+            Assert.AreEqual(50, vehicle2.BatteryLevel);
+            Assert.AreEqual(50, vehicle3.BatteryLevel);
+            Assert.AreEqual(resultDamage, true);
+        }
+
+        [Test]
+        public void RepairVehicleShouldRepairAllDamageVehicles()
+        {
+            Garage garage = new Garage(5);
+
+            Vehicle vehicle = new Vehicle("Nisan", "GTR", "BS5555BS");
+            Vehicle vehicle2 = new Vehicle("Nisan", "Godzile", "BS5655BS");
+            Vehicle vehicle3 = new Vehicle("Nisan", "Aplha12", "BS5557BS");
+            Vehicle vehicle4 = new Vehicle("Nisan", "Aplha16", "BS5577BS");
+            Vehicle vehicle5 = new Vehicle("Nisan", "Aplha18", "BS5584BS");
+
+            garage.AddVehicle(vehicle);
+            garage.AddVehicle(vehicle2);
+            garage.AddVehicle(vehicle3);
+            garage.AddVehicle(vehicle4);
+            garage.AddVehicle(vehicle5);
+
+
+            garage.DriveVehicle(vehicle.LicensePlateNumber, 30, true);
+            garage.DriveVehicle(vehicle2.LicensePlateNumber, 50, true);
+            garage.DriveVehicle(vehicle3.LicensePlateNumber, 50, true);
+
+            var resultRepair = garage.RepairVehicles();
+            var allRepairVehicles = garage.Vehicles.Any(x => !x.IsDamaged);
+
+            Assert.AreEqual($"Vehicles repaired: 3", resultRepair);
+            Assert.IsTrue(allRepairVehicles);
+        }
     }
 }
