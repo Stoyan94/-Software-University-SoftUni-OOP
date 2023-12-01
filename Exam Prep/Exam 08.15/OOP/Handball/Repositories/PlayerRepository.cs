20aaf3d855f28ad27f805d7f38a2957678839e49
Stoqn <podlobg@gmail.com>
2023-12-01 18:12:35 +0200

    up

---
+++ Exam Prep/Exam 08.15/OOP/Handball/Repositories/PlayerRepository.cs
@@ -0,0 +1,63 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private readonly ICollection<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public IReadOnlyCollection<IPlayer> Models => (IReadOnlyCollection<IPlayer>) players;
+
+        public void AddModel(IPlayer model)
+        {
+            players.Add(model);
+        }
+
+        public bool ExistsModel(string name)
+        {
+            var ExistingPlayer = players.FirstOrDefault(p => p.Name == name);
+
+            if (ExistingPlayer is null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public IPlayer GetModel(string name)
+        {
+            var returnPlayer = players.FirstOrDefault(p => p.Name == name);
+
+            if (returnPlayer is null)
+            {
+                return null;
+            }
+            return true;
+        }
+    
+
+        public bool RemoveModel(string name)
+        {
+            var removePlayer = players.FirstOrDefault(p => p.Name == name);
+
+            if (removePlayer is null)
+            {
+                return false;
+            }
+
+            players.Remove(removePlayer);
+            return true;
+        }
+    }
+}
---
+++ Exam Prep/Exam 08.15/OOP/Handball/Repositories/TeamRepository.cs
@@ -0,0 +1,55 @@
+﻿using Handball.Models;
+using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class TeamRepository : IRepository<ITeam>
+    {
+        private readonly ICollection<ITeam> team;
+
+        public TeamRepository()
+        {
+            this.team = new List<ITeam>();
+        }
+
+        public IReadOnlyCollection<ITeam> Models => (IReadOnlyCollection<ITeam>) team;
+
+        public void AddModel(ITeam model)
+        {
+            team.Add(model);
+        }
+
+        public bool ExistsModel(string name)
+        {
+            var existingTeam = team.FirstOrDefault(p => p.Name == name);
+
+            if (existingTeam is null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public ITeam GetModel(string name)
+        {
+            var returnTeam = team.FirstOrDefault(p => p.Name == name);
+
+            if (returnTeam is null)
+            {
+                return null;
+            }
+            return true;
+        }
+
+        public bool RemoveModel(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
