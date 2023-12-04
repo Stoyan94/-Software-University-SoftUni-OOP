6fa77f442c3a471c4699d1ed42c7feb9991b9454
Stoqn <podlobg@gmail.com>
2023-12-04 14:23:48 +0200

    up

--- Exam Prep/Exam 08.15/Unit Testring/SmartDevice.Tests/UnitTest1.cs
+++ Exam Prep/Exam 08.15/Unit Testring/SmartDevice.Tests/UnitTest1.cs
@@ -6,15 +6,35 @@ namespace SmartDevice.Tests
 
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+      
+        [Test]
+        public void CtorShouldInitilazieCorrectly()
         {
+            int excpetedMemory = 10;
+            int avaibleMemory = 5;
+
+            Device device = new Device(excpetedMemory);
+            device.TakePhoto(5);
+            int photo = device.Photos;
+
+            Assert.AreEqual(excpetedMemory, device.MemoryCapacity);
+            Assert.AreEqual(avaibleMemory, device.AvailableMemory);
         }
 
-        [Test]
-        public void Test()
+        [Test] 
+
+        public void InstallAppMethodShouldWorkCorrectly()
         {
-            Assert.Pass();
+            Device device = new Device(50);
+
+            string appName = "TestApp";
+            int appSize = 20;
+
+            device.InstallApp(appName,appSize);
+
+            Assert.AreNotEqual(device.MemoryCapacity, appSize);
+            Assert.IsTrue(device.Applications.Contains(appName));
+            
         }
     }
 }
