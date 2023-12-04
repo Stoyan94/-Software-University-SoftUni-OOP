85842c58b62b32a00bd9699e7c3c3cb004134f04
Stoqn <podlobg@gmail.com>
2023-12-04 15:04:37 +0200

    up

--- Exam Prep/Exam 08.15/Unit Testring/SmartDevice.Tests/UnitTest1.cs
+++ Exam Prep/Exam 08.15/Unit Testring/SmartDevice.Tests/UnitTest1.cs
@@ -30,11 +30,47 @@ namespace SmartDevice.Tests
             string appName = "TestApp";
             int appSize = 20;
 
-            device.InstallApp(appName,appSize);
+             string retur = device.InstallApp(appName,appSize);
 
             Assert.AreNotEqual(device.MemoryCapacity, appSize);
             Assert.IsTrue(device.Applications.Contains(appName));
-            
+            Assert.AreEqual(retur, "TestApp is installed successfully. Run application?");
+        }
+
+        [Test]
+
+        public void InstallAppMethodShoulThrowException()
+        {
+            Device device = new Device(50);
+
+            string appName = "TestApp";
+            int appSize = 100;         
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                device.InstallApp(appName, appSize);
+            }, "Not enough available memory to install the app.");
+        }
+
+        [Test] 
+        public void TakePhotoShouldAddCorrectlyPhoto()
+        {
+            Device device = new Device(50);
+
+            Assert.That(device.TakePhoto(20));
+            Assert.IsFalse(device.TakePhoto(60));
+        }
+
+        [Test]
+
+        public void FormatDeviceShouldWorkCorrectly()
+        {
+            Device device = new Device(50);
+            device.FormatDevice();            
+
+            Assert.That(device.Photos == 0);
+            Assert.IsTrue(device.AvailableMemory == device.MemoryCapacity);
+          
         }
     }
 }
