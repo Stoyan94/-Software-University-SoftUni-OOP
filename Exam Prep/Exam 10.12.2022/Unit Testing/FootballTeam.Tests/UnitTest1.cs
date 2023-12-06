43ab7189dbaf2e5d976ae7d0de2fd528b28b3504
Stoqn <podlobg@gmail.com>
2023-12-06 13:08:17 +0200

    up

--- Exam Prep/Exam 10.12.2022/Unit Testing/FootballTeam.Tests/UnitTest1.cs
+++ Exam Prep/Exam 10.12.2022/Unit Testing/FootballTeam.Tests/UnitTest1.cs
@@ -1,18 +1,64 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace FootballTeam.Tests
 {
     public class Tests
-    {
-        [SetUp]
-        public void Setup()
+    {        
+        [Test]
+        public void FootbalTeamConstructorShouldWorkCorrectly()
         {
+            string expectedTeamName = "pokemons";
+            int expectedTeamCap = 22;
+
+            FootballTeam team = new FootballTeam(expectedTeamName, expectedTeamCap);
+
+            List<FootballPlayer> players = new List<FootballPlayer>();
+
+            FootballPlayer player = new FootballPlayer("stqon",5, "Midfielder");
+            FootballPlayer player2 = new FootballPlayer("pesho",10, "Forward");
+
+            players.Add(player);
+            players.Add(player2);
+
+            Assert.AreEqual(expectedTeamName, team.Name);
+            Assert.AreEqual(22, team.Capacity);
+            Assert.True(players.Contains(player));
+            Assert.True(players.Contains(player2));
+           
         }
 
         [Test]
-        public void Test1()
+
+        public void TeamCapacityShouldThrowExceptionWheNIsLessThen15()
         {
-            Assert.Pass();
+            string expectedTeamName = "pokemons";
+            int expectedTeamCap = 14;            
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                FootballTeam team = new FootballTeam(expectedTeamName, expectedTeamCap);
+            }, "Capacity min value = 15");
+        }
+
+        [Test]
+
+        public void TeamNameShouldThrowExceptioIsNullOrEmpty()
+        {
+            string expectedTeamName = "";
+            string expectedTeamName1 = null;
+            int expectedTeamCap = 14;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                FootballTeam team = new FootballTeam(expectedTeamName, expectedTeamCap);
+            }, "Name cannot be null or empty!");
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                FootballTeam team = new FootballTeam(expectedTeamName1, expectedTeamCap);
+            }, "Name cannot be null or empty!");
         }
     }
 }
