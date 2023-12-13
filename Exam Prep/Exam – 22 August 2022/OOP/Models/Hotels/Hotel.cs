9c9c71b25044c58bcab84dabbe97160850948953
Stoqn <podlobg@gmail.com>
2023-12-13 18:55:57 +0200

    up

---
+++ Exam Prep/Exam – 22 August 2022/OOP/Models/Hotels/Hotel.cs
@@ -0,0 +1,58 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        private string fullName;
+        private int category;
+
+        public Hotel(string fullName, int category)
+        {
+            FullName = fullName;
+            Category = category;
+        }
+
+        public string FullName
+        {
+            get => fullName; 
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hotel name cannot be null or empty!");
+                }
+
+                fullName = value;
+            }
+        }
+
+
+        public int Category
+        {
+            get => category; 
+            private set
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentException("Category should be between 1 and 5 stars!");
+                }
+                category = value;
+            }
+        }
+
+        public double Turnover => throw new NotImplementedException();
+
+        public IRepository<IRoom> Rooms => throw new NotImplementedException();
+
+        public IRepository<IBooking> Bookings => throw new NotImplementedException();
+    }
+}
---
+++ Exam Prep/Exam – 22 August 2022/OOP/Repositories/RoomRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories
+{
+    public class RoomRepository : IRepository<IRoom>
+    {
+        private readonly ICollection<IRoom> rooms;
+
+        public RoomRepository()
+        {
+            rooms = new List<IRoom>();
+        }
+        public void AddNew(IRoom model) => rooms.Add(model);
+
+        public IRoom Select(string criteria)
+        {
+            var room = rooms.FirstOrDefault(r => r.GetType().Name == criteria);
+
+            if (room == null)
+            {
+                return null;
+            }
+            return room;
+        }
+
+        public IReadOnlyCollection<IRoom> All() => rooms.ToList().AsReadOnly();
+
+    }
+}
