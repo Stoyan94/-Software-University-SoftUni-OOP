3050eba9d570501262bbb0f69cedc8cb44cdbc08
Stoqn <podlobg@gmail.com>
2023-12-23 13:18:35 +0200

    up

---
+++ Workshop-Snake/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    internal class Wall : GameObject
+    {
+        private const char WallSymbol = '\u25A0';
+        public Wall(char drawSymbol) 
+            : base(drawSymbol)
+        {
+            
+        }
+
+        public Wall(char drawSymbol, int x, int y) 
+            : base(drawSymbol, x, y)
+        {
+            DrawHorizontalLine(0);
+        }
+
+        private void DrawHorizontalLine(int colum)
+        {
+            for (int row = 0; row < X; row++)
+            {
+                GameObject drawPoint = new GameObject(drawSymbol, X, colum);
+            }
+        }
+    }
+}
