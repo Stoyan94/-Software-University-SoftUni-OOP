8e73d654a0fa6f28a7504120970ab559fefd2204
Stoqn <podlobg@gmail.com>
2023-12-23 14:08:06 +0200

    up

---
+++ Workshop-Snake/SimpleSnake/GameObjects/Food/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleSnake.GameObjects.Food
+{
+    internal class Food : GameObject
+    {
+        public Food(char drawSymbol)
+            : base(drawSymbol)
+        {
+        }
+
+        public Food(char drawSymbol, int x, int y)
+            : base(drawSymbol, x, y)
+        {
+        }
+
+        public int Points { get; set; }
+    }
+}
