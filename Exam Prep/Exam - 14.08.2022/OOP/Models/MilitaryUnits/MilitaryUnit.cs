34fbb63e0244ac3cfc1dab158c612828c4906b14
Stoqn <podlobg@gmail.com>
2024-01-12 12:57:08 +0200

    up

---
+++ Exam Prep/Exam - 14.08.2022/OOP/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,28 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        public MilitaryUnit(double cost)
+        {
+            Cost = cost;
+        }
+        public double Cost { get; private set; }
+
+        public int EnduranceLevel { get; private set; }
+
+        public void IncreaseEndurance()
+        {
+            EnduranceLevel++;
+
+            if (EnduranceLevel > 20)
+            {
+                EnduranceLevel = 20;
+            }
+        }
+    }
+}
--- Exam Prep/Exam - 14.08.2022/OOP/Models/Weapons/Weapon.cs
+++ Exam Prep/Exam - 14.08.2022/OOP/Models/Weapons/Weapon.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using PlanetWars.Models.Weapons.Contracts;
+using System;
 
 namespace PlanetWars.Models.Weapons
 {
-    internal class Weapon
+    public abstract class Weapon : IWeapon
     {
+        private int destructionLevel;
+        protected Weapon(int destructionLevel, double price)
+        {
+            DestructionLevel = destructionLevel;
+            Price = price;
+        }
+
+        public double Price { get; private set; }
+
+        public int DestructionLevel
+        {
+            get => destructionLevel; 
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Destruction level cannot be zero or negative.");
+                }
+                else if (value > 10)
+                {
+                    throw new ArgumentException("Destruction level cannot exceed 10 power points.");
+                }
+                destructionLevel = value;
+            }
+        }
+
     }
 }
