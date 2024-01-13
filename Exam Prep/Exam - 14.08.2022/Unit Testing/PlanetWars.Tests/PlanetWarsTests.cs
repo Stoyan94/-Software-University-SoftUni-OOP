0666a0b264486809de5aae77986f501a45d2c50e
Stoqn <podlobg@gmail.com>
2024-01-13 19:46:41 +0200

    up

--- Exam Prep/Exam - 14.08.2022/Unit Testing/PlanetWars.Tests/PlanetWarsTests.cs
+++ Exam Prep/Exam - 14.08.2022/Unit Testing/PlanetWars.Tests/PlanetWarsTests.cs
@@ -46,6 +46,51 @@ namespace PlanetWars.Tests
                 Assert.That(weapon.Price, Is.EqualTo(120));
                 Assert.That(weapon.Name, Is.EqualTo("Nuclear"));
             }
+            [Test]
+            public void AddWeapon_WeaponIsAddedToPlanetCollectionOfWeapons()
+            {
+                var planet = new Planet("Venus", 120);
+                var weapon = new Weapon("Nuclear", 100, 3);
+
+                planet.AddWeapon(weapon);
+
+                Assert.That(planet.Weapons.Count, Is.EqualTo(1));
+            }
+
+            [Test]
+            public void AddWeapon_AlreadyAddedWeapon()
+            {
+                var planet = new Planet("Venus", 120);
+                var weapon = new Weapon("Nuclear", 20, 12);
+
+                planet.AddWeapon(weapon);
+
+                Assert.Throws<InvalidOperationException>(
+                () => planet.AddWeapon(weapon),
+                $"There is already a {weapon.Name} weapon");
+            }
+
+            [Test]
+            public void DestructionLevel_DestructionLevelIsReturned()
+            {
+                var planet = new Planet("Venus", 80);
+                var weapon = new Weapon("Nuclear", 30, 3);
+                var weaponTwo = new Weapon("SpaceMissiles", 20, 2);
+
+                planet.AddWeapon(weapon);
+                planet.AddWeapon(weaponTwo);
+
+                Assert.That(planet.MilitaryPowerRatio, Is.EqualTo(5));
+            }
+
+            [Test]
+            public void Profit_BudgetIncreasesWithGivenAmount()
+            {
+                var planet = new Planet("Venus", 80);
+                planet.Profit(33);
+
+                Assert.That(planet.Budget, Is.EqualTo(113));
+            }
         }
     }
 }
